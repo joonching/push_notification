f22b4c02ac4867769fa7e252336f5935136fb93e
Josh Chung <Josh Chung>
2017-04-17 20:39:46 -0400

    can build APK and adding FCM testing outputs

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ This is my Push Notification portion for final project in ECE 4574 at Virginia T
 * Android Studio
 * Your device is connected to the internet (to clone the repository and download required libraries)
 
+## Firebase Cloud Messaging Automated Testing Outputs ##
+[Nexus 5](https://00e9e64bac42cda73b8f8b312788b5cd765795073a8aa76fcd-apidata.googleusercontent.com/download/storage/v1/b/test-lab-b44s4yat051uz-hckq2t4i7yd2m/o/web-build_2017-04-18T00:17:45.727Z_EIqR%2FNexus5X-25-en_US-portrait%2Fbugreport.txt?qk=AD5uMEuaJA-G0TptZ2TNWYHpnwwpkB2IAPM-ORTwhjBQn3G_1fwdpazVhFRamYuOGWU786HUT_yigqte1GLU68FDXpKYoBtldXJw6F_NSTtFkec8wiy97J4CcFBbcZikyKDQoS96oNQuUZUSfTOgExMG4bPEC9jPCQWpdvZpnvh2rBiK6Og2DdyUhOCZ7wZRtVU60VWD2VBoKtmukR0Zkc3Ki55rl0Ry7JHTaAUYudQs_O0iOC-bPypiWcbLLCkn2JQKhox98Ifz4LUVSAPvjWqjPGDlYDoQxWLQXf6jzPy4D4h-AHAN-iUmyrsnWvnK4F4gBa9HmkyorvX4gFNfFqWCE27iwa3647kTn3_Wl8BOBVP511SGtiwQWmd3jKELsRlj2KWsMINVVf4BR3Kd4OB9kKPalWb5CGuql0NhYJ4ls20sT8e61ErRopcjyS-8SqBcPXFNk8aF-8We7d_RWEw0Qf9KhQiyUwhuiqcaoVBrsw9zqQi8kd5P4OXVWhb5ss5-F_ccpDeE-bYwerltal9uR8Fi8zpYAIVLISrL2NRTyw33CdBTigD7CeGyTybwd7CIDLSJYuvTkhKBMIctFmVNV_LMM3TRaN9T35HFi6GLU_otyurvT2LrXx4PhZeCEafiQmuvBhyzPoWFONeMFgrX5hnDhRMzLqsnbXQGzBMHxieFD1fnoOI8owPIHL-phaHT70-PxXc7LLAWye12wDBx6YhykQiq6Pq7UDYel8GnDBdXtGLlknJUhQb7mR79DnvErcWVfgzmVI3PKEFrcwpQEpob2C3yZHo0RZvSgfNBlRhbLy0IvigYXJB8Mz4rVk5b3X97b-dHQLySZMaaNuf3gi-1kcbHynIkxN5xGZQjoJZQia2ksIoJw3FrZDRcz06de_plNAQm)
+
 ## How to run the test ##
 1. go ahead and gitclone this repo
 `git clone https://github.com/joonching/push_notification.git`
@@ -27,6 +30,10 @@ When the application gets fully implemented then the registration_id will start 
 
 If you want to see the actual application on a phone then you would have to run the Android Studio project and run the virtual phone
 As of now the test is getting run to my current Firebase cloud messaging server that reroutes the messages to my virtual phone. 
+
+
+### Edits ###
+1. Improved testing capability to more than one phone - therefore the image is out of date
 
 ### Authors ###
 
--- send.py
+++ send.py
@@ -31,6 +31,8 @@ from pyfcm import FCMNotification
 import datetime
 api_key = sys.argv[1]
 registration_id = sys.argv[2]
+reg_id = sys.argv[3]
+reg_ids = [sys.argv[2], sys.argv[3]]
 push_service = FCMNotification(api_key=api_key)
 
 # OR initialize with proxies
@@ -59,7 +61,7 @@ print()
 print("**TEST START**")
 print()
 
-print("Single text Title and Body")
+print("Single text Title and Body to Single Phone")
 
 a = datetime.datetime.now()
 result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
@@ -76,7 +78,24 @@ except AssertionError:
 	print("Failure, Performance Time: " + str(c.total_seconds()) + "s")
 print()
 
-print("Null text Title and Body")
+print("Single text Title and Body to Multiple Phones")
+
+a = datetime.datetime.now()
+result = push_service.notify_multiple_devices(registration_ids=reg_ids, message_title=message_title, message_body=message_body)
+b = datetime.datetime.now()
+c = b - a
+total_test_time = c.total_seconds() + total_test_time;
+try: 
+	assert result[0]['success'] == 2
+	print("Success, Performance Time: " + str(c.total_seconds()) + "s")
+	total_push = total_push + 1
+	passed_test = passed_test + 1;
+except AssertionError:
+	failed_test = failed_test + 1
+	print("Failure, Performance Time: " + str(c.total_seconds()) + "s")
+print()
+
+print("Null text Title and Body to Single Phone")
 
 a = datetime.datetime.now()
 result = push_service.notify_single_device(registration_id=registration_id, message_title="\0", message_body="\0")
@@ -93,7 +112,25 @@ except AssertionError:
 	print("Failure, Performance Time: " + str(c.total_seconds()) + "s")
 print()
 
-print("Multiple text Title and Body")
+print("Null text Title and Body to Multiple Phones")
+
+a = datetime.datetime.now()
+result = push_service.notify_multiple_devices(registration_ids=reg_ids, message_title="\0", message_body="\0")
+b = datetime.datetime.now()
+c = b - a
+total_test_time = c.total_seconds() + total_test_time;
+try: 
+	assert result[0]['success'] == 2
+	print("Success, Performance Time: " + str(c.total_seconds()) + "s")
+	total_push = total_push + 1
+	passed_test = passed_test + 1;
+except AssertionError:
+	failed_test = failed_test + 1
+	print("Failure, Performance Time: " + str(c.total_seconds()) + "s")
+print()
+
+
+print("Multiple text Title and Body to Single Phone")
 
 a = datetime.datetime.now()
 failure_bool = 0
@@ -114,6 +151,30 @@ else:
 	passed_test = passed_test + 1;
 	print("Success, Performance Time: " + str(c.total_seconds()) + "s")
 	print()
+
+
+print("Multiple text Title and Body to Multiple Phones")
+
+a = datetime.datetime.now()
+failure_bool = 0
+for num in range(0,20):
+	result = push_service.notify_multiple_devices(registration_ids=reg_ids, message_title=message_title, message_body=message_body)
+	try: 
+		assert result[0]['success'] == 2
+		total_push = total_push + 1
+	except AssertionError:
+		failure_bool = 1
+b = datetime.datetime.now()
+c = b - a
+total_test_time = c.total_seconds() + total_test_time;
+if failure_bool == 1:
+	failed_test = failed_test + 1;
+	print("Failure, Performance Time: " + str(c.total_seconds()) + "s")
+else:
+	passed_test = passed_test + 1;
+	print("Success, Performance Time: " + str(c.total_seconds()) + "s")
+	print()
+
 
 print("Single number Title and Body")
 
@@ -176,7 +237,7 @@ except AssertionError:
 print()
 
 
-print("Send Special Characters")
+print("Send Special Characters to Single Phone")
 
 a = datetime.datetime.now()
 failure_bool = 0
@@ -184,6 +245,28 @@ for num in range(len(special_chars)):
 	result = push_service.notify_single_device(registration_id=registration_id, message_title=special_chars[num], message_body=special_chars[num])
 	try: 
 		assert result['success'] == 1
+		total_push = total_push + 1
+	except AssertionError:
+		failure_bool = 1
+b = datetime.datetime.now()
+c = b - a
+total_test_time = c.total_seconds() + total_test_time;
+if failure_bool == 1:
+	failed_test = failed_test + 1;
+	print("Failure, Performance Time: " + str(c.total_seconds()) + "s")
+else:
+	passed_test = passed_test + 1;
+	print("Success, Performance Time: " + str(c.total_seconds()) + "s")
+	print()
+
+print("Send Special Characters to Multiple Phones")
+
+a = datetime.datetime.now()
+failure_bool = 0
+for num in range(len(special_chars)):
+	result = push_service.notify_multiple_devices(registration_ids=reg_ids, message_title=special_chars[num], message_body=special_chars[num])
+	try: 
+		assert result[0]['success'] == 2
 		total_push = total_push + 1
 	except AssertionError:
 		failure_bool = 1
@@ -219,7 +302,7 @@ print()
 
 
 print("Total Test Time: " + str(total_test_time))
-print("Total Tests Ran: X")
+print("Total Tests Ran: 12")
 print("Total Successful Push notifications sent: " + str(total_push))
 print("Total Tests Passed: " + str(passed_test))
 print("Total Tests Failed: " + str(failed_test))
